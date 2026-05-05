eba9c37575c5bd0259c43f6eade173aa47732697
pabersilalahi02-hub <pabersilalahi02@gmail.com>
2026-05-05 12:55:29 +0700

    Membuat prodi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\PeriodeController;
 use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
@@ -14,3 +15,4 @@ Route::resource('fakultas', FakultasController::class);
 Route::resource('periode', PeriodeController::class);
 
 Route::resource('berita', BeritaController::class);
+Route::get('/prodi', [ProdiController::class, 'index']);
